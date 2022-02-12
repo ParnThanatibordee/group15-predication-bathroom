068293ecdbf8184dbd1fdc1e423c7a8cdef0cec3
Kop <copter1144@hotmail.com>
2022-02-12 16:55:51 +0700

    resolve cores

--- backend-bathroom.py
+++ backend-bathroom.py
@@ -6,10 +6,21 @@ from typing import Optional
 from fastapi.encoders import jsonable_encoder
 import json
 import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 from pymongo import MongoClient
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient('mongodb://localhost', 27017)
 db = client["Bathroom"]
